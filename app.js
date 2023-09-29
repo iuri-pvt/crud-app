8b65711f19d4590b7b389f8a6619eba203281a6d
iuri <dasilvaiuri21@gmail.com>
2023-09-29 18:47:32 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+const dataFilePath = 'data.json';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Página inicial
+app.get('/', (req, res) => {
+  const data = loadData();
+  res.render('index.ejs', { data });
+});
+
+// Adicionar um novo registro
+app.post('/add', (req, res) => {
+  const newData = {
+    nome: req.body.nome,
+    idade: req.body.idade,
+  };
+
+  const data = loadData();
+  data.push(newData);
+  saveData(data);
+
+  res.redirect('/');
+});
+
+// Excluir um registro
+app.get('/delete/:index', (req, res) => {
+  const index = req.params.index;
+  const data = loadData();
+  if (index >= 0 && index < data.length) {
+    data.splice(index, 1);
+    saveData(data);
+  }
+  res.redirect('/');
+});
+
+// Página de edição de registro
+app.get('/edit/:index', (req, res) => {
+  const index = req.params.index;
+  const data = loadData();
+  if (index >= 0 && index < data.length) {
+    res.render('edit.ejs', { index, record: data[index] });
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Atualizar registro
+app.post('/update/:index', (req, res) => {
+  const index = req.params.index;
+  const data = loadData();
+  if (index >= 0 && index < data.length) {
+    data[index] = {
+      nome: req.body.nome,
+      idade: req.body.idade,
+    };
+    saveData(data);
+  }
+  res.redirect('/');
+});
+
+// Função para carregar dados do arquivo JSON
+function loadData() {
+  try {
+    const data = fs.readFileSync(dataFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+}
+
+// Função para salvar dados no arquivo JSON
+function saveData(data) {
+  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando em http://localhost:${port}`);
+});
